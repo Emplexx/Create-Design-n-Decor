66b7b62acbfbf4f4f3b7bbb424f1603146559840
Emplexx <ishchuk132@gmail.com>
2025-03-03 16:24:40 +0100

    Partial ceiling fan port (currently has an alpha issue with the model)

--- src/main/java/com/mangomilk/design_decor/blocks/ceiling_fan/CeilingFanVisual.java
+++ src/main/java/com/mangomilk/design_decor/blocks/ceiling_fan/CeilingFanVisual.java
@@ -7,62 +7,92 @@ import com.simibubi.create.content.kinetics.base.RotatingInstance;
 import com.simibubi.create.foundation.render.AllInstanceTypes;
 import dev.engine_room.flywheel.api.instance.Instance;
 import dev.engine_room.flywheel.api.visualization.VisualizationContext;
+import dev.engine_room.flywheel.lib.instance.TransformedInstance;
 import dev.engine_room.flywheel.lib.model.Models;
-import org.jetbrains.annotations.Nullable;
+import dev.engine_room.flywheel.lib.visual.SimpleDynamicVisual;
+import net.createmod.catnip.math.AngleHelper;
+import net.minecraft.core.Direction;
+import org.joml.Matrix4f;
+import org.joml.Quaternionf;
 
 import java.util.function.Consumer;
 
-// TODO large ass todo
-
-public class CeilingFanVisual extends KineticBlockEntityVisual<CeilingFanBlockEntity> {
+public class CeilingFanVisual extends KineticBlockEntityVisual<CeilingFanBlockEntity> implements SimpleDynamicVisual {
 
     protected final RotatingInstance shaft;
-    protected final RotatingInstance fan;
+    protected final TransformedInstance fan;
+    protected float lastAngle = Float.NaN;
+
+    protected final Matrix4f baseTransform = new Matrix4f();
 
     public CeilingFanVisual(VisualizationContext context, CeilingFanBlockEntity blockEntity, float partialTick) {
         super(context, blockEntity, partialTick);
 
-        fan = instancerProvider()
-                .instancer(AllInstanceTypes.ROTATING, Models.partial(CDDPartialModels.CEILING_FAN))
-                .createInstance();
+        var axis = rotationAxis();
         shaft = instancerProvider().instancer(AllInstanceTypes.ROTATING, Models.partial(AllPartialModels.SHAFT_HALF))
                 .createInstance();
+        shaft.setup(blockEntity)
+                .setPosition(getVisualPosition())
+                .rotateToFace(Direction.SOUTH, Direction.DOWN.getOpposite())
+                .setChanged();
 
+        fan = instancerProvider().instancer(AllInstanceTypes.SCROLLING_TRANSFORMED, Models.partial(CDDPartialModels.CEILING_FAN))
+                .createInstance();
 
+        Direction align = Direction.fromAxisAndDirection(axis, Direction.AxisDirection.POSITIVE);
+        fan.translate(getVisualPosition())
+                .center()
+                .rotate(new Quaternionf().rotateTo(0, 1, 0, align.getStepX(), align.getStepY(), align.getStepZ()));
 
+        baseTransform.set(fan.pose);
 
+        animate(blockEntity.angle);
+    }
+
+    @Override
+    public void beginFrame(Context context) {
+
+        float partialTicks = context.partialTick();
+
+        float speed = blockEntity.visualSpeed.getValue(partialTicks) * 3 / 10f;
+        float angle = blockEntity.angle + speed * partialTicks;
+
+        if (Math.abs(angle - lastAngle) < 0.001)
+            return;
+
+        animate(angle);
+
+        lastAngle = angle;
     }
 
     private void animate(float angle) {
-//        PoseStack ms = new PoseStack();
-//        TransformStack msr = TransformStack.of(ms);
-
-//        msr.translate(getVisualPosition());
-//        msr.center()
-//
-//                .rotate(Direction.get(Direction.AxisDirection.POSITIVE, axis), AngleHelper.rad(angle))
-//                .unCentre();
+        fan.setTransform(baseTransform)
+                .rotateY(AngleHelper.rad(angle))
+                .uncenter()
+                .setChanged();
     }
 
     @Override
-    public void collectCrumblingInstances(Consumer<@Nullable Instance> consumer) {
-        consumer.accept(shaft);
-        consumer.accept(fan);
+    public void update(float pt) {
+        shaft.setup(blockEntity)
+                .setChanged();
     }
 
     @Override
-    public void update(float partialTick) {
-        fan.setup(blockEntity).setChanged();
-    }
-
-    @Override
-    public void updateLight(float v) {
-
+    public void updateLight(float partialTick) {
+        relight(shaft, fan);
     }
 
     @Override
     protected void _delete() {
+        shaft.delete();
         fan.delete();
+    }
+
+    @Override
+    public void collectCrumblingInstances(Consumer<Instance> consumer) {
+        consumer.accept(shaft);
+        consumer.accept(fan);
     }
 
 }
